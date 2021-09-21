39decedc6d78c012b6eee1b617036fdd56dc1ad9
Andej Nesterenko <mentatus.n@gmail.com>
2021-09-21 23:13:18 +0300

    8 урок ФАКУЛЬТАТИВ (Миграция версий БД)

--- app/src/main/java/ru/geekbrains/lesson_1423_2_2_main/room/HistoryDataBase.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_2_main/room/HistoryDataBase.kt
@@ -2,8 +2,11 @@ package ru.geekbrains.lesson_1423_2_2_main.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = arrayOf(HistoryEntity::class),version = 1,exportSchema = false)
+@Database(entities = arrayOf(HistoryEntity::class),version = 4,exportSchema = false)
 abstract class HistoryDataBase: RoomDatabase() {
     abstract fun historyDAO():HistoryDAO
 }
+
