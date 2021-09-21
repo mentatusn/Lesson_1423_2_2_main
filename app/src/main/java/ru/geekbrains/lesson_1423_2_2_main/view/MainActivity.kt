766eba833d366c9593391e53f3aa38a8872cc9b6
Andej Nesterenko <mentatus.n@gmail.com>
2021-09-21 21:02:15 +0300

    8 урок начали

--- app/src/main/java/ru/geekbrains/lesson_1423_2_2_main/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/lesson_1423_2_2_main/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.geekbrains.lesson_1423_2_2_main.view
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,7 @@ import ru.geekbrains.lesson_1423_2_2_main.R
 import ru.geekbrains.lesson_1423_2_2_main.databinding.ActivityMainBinding
 import ru.geekbrains.lesson_1423_2_2_main.lesson6.ThreadsFragment
 import ru.geekbrains.lesson_1423_2_2_main.view.main.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         if (savedInstanceState == null)
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, MainFragment.newInstance()).commit()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
